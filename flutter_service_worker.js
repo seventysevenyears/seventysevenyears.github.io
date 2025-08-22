9fa55398936f18c8dcf6116bb32392adea377a62
Chung <Chung@gmail.com>
2025-08-22 15:12:10 +0900

    폰트 수정

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "3c57b0e71d0eef73deffff79e7a1e678",
+"flutter_bootstrap.js": "88119aecb968e0055c78309f91366d21",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "bee61ab19139d6ed2fa428ac615685fe",
 "/": "bee61ab19139d6ed2fa428ac615685fe",
-"main.dart.js": "4310a8855bcd8421d23ae03eba2319b2",
+"main.dart.js": "6a3ba3eead50a92c722d2ff1c315e518",
 "manifest.json": "e229325643a60b3c6a89c54fac1e49e9",
 "version.json": "027975ae79106b3150f050e262a09a94"};
 // The application shell files that are downloaded before a service worker can
